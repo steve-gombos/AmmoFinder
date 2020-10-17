e605201012afb894775f8707181ca9cc56e58309
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2020-10-17 00:05:09 -0400

    Added startup tests for functions.

--- AmmoFinder.Functions.UnitTests/RefreshRetailerProductsTests.cs
+++ AmmoFinder.Functions.UnitTests/RefreshRetailerProductsTests.cs
@@ -1,7 +1,13 @@
 using AmmoFinder.Common.Interfaces;
 using AmmoFinder.Testing.Extensions;
+using AngleSharp;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Moq;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace AmmoFinder.Functions.UnitTests
@@ -21,6 +27,22 @@ namespace AmmoFinder.Functions.UnitTests
 
             // Assert
             mockedLogger.VerifyLog(LogLevel.Information, "C# Timer trigger function executed.", Times.Once());
+        }
+
+        [Fact]
+        public void Function_CatchesException()
+        {
+            // Arrange
+            var mockedLogger = new Mock<ILogger<RefreshRetailerProducts>>();
+            var mockedRefreshProducts = new Mock<IRefreshProducts>();
+            mockedRefreshProducts.Setup(rf => rf.Refresh()).Throws(new Exception("Error"));
+            var refreshRetailerProductsFunction = new RefreshRetailerProducts(mockedRefreshProducts.Object);
+
+            // Act
+            refreshRetailerProductsFunction.Run(null, mockedLogger.Object);
+
+            // Assert
+            mockedLogger.VerifyLog(LogLevel.Error, "Error", Times.Once());
         }
     }
 }
---
+++ AmmoFinder.Functions.UnitTests/StartupTests.cs
@@ -0,0 +1,48 @@
+﻿using AmmoFinder.Common.Interfaces;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AmmoFinder.Functions.UnitTests
+{
+    public class StartupTests
+    {
+        [Fact]
+        public void Startup_ConfigurationBuilder_IsValid()
+        {
+            // Arrange
+            var startup = new Startup();
+            var mockedBuilder = new Mock<IFunctionsConfigurationBuilder>();
+            var mockedConfiguration = new ConfigurationBuilder();
+            mockedBuilder.Setup(b => b.ConfigurationBuilder).Returns(mockedConfiguration);
+
+            // Act
+            startup.ConfigureAppConfiguration(mockedBuilder.Object);
+
+            // Assert
+            Assert.True(mockedConfiguration.Sources.Any());
+        }
+
+        [Fact]
+        public void Startup_Configure_IsValid()
+        {
+            // Arrange
+            var startup = new Startup();
+            var mockedBuilder = new Mock<IFunctionsHostBuilder>();
+            var services = new ServiceCollection();
+            mockedBuilder.Setup(b => b.Services).Returns(services);
+            startup.Configure(mockedBuilder.Object);
+            var provider = services.BuildServiceProvider();
+
+            // Act
+            var productServices = provider.GetRequiredService<IEnumerable<IProductService>>();
+
+            // Assert
+            Assert.True(productServices.Count() == 3);
+        }
+    }
+}
