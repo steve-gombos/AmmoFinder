5c5c0ecfcdc776fa7cebbefe45ec5603fee0201b
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2020-10-25 17:04:23 -0400

    Cleanup code.

--- src/AmmoFinder.Common/Converters/BoolConverter.cs
+++ src/AmmoFinder.Common/Converters/BoolConverter.cs
@@ -1,20 +1,12 @@
-﻿using System.Buffers;
-using System.Buffers.Text;
-
-namespace System.Text.Json.Serialization
+﻿namespace System.Text.Json.Serialization
 {
     public class BoolConverter : JsonConverter<bool>
     {
         public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.TokenType == JsonTokenType.String)
+            if (reader.TokenType == JsonTokenType.String && bool.TryParse(reader.GetString(), out var boolean))
             {
-                ReadOnlySpan<byte> span = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
-                if (Utf8Parser.TryParse(span, out bool boolean, out int bytesConsumed) && span.Length == bytesConsumed)
-                    return boolean;
-
-                if (bool.TryParse(reader.GetString(), out boolean))
-                    return boolean;
+                return boolean;
             }
 
             return reader.GetBoolean();
--- src/AmmoFinder.Common/Converters/LongConverter.cs
+++ src/AmmoFinder.Common/Converters/LongConverter.cs
@@ -1,20 +1,12 @@
-﻿using System.Buffers;
-using System.Buffers.Text;
-
-namespace System.Text.Json.Serialization
+﻿namespace System.Text.Json.Serialization
 {
     public class LongConverter : JsonConverter<long>
     {
         public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.TokenType == JsonTokenType.String)
+            if (reader.TokenType == JsonTokenType.String && long.TryParse(reader.GetString(), out var number))
             {
-                //ReadOnlySpan<byte> span = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
-                //if (Utf8Parser.TryParse(span, out long number, out int bytesConsumed) && span.Length == bytesConsumed)
-                //    return number;
-
-                if (long.TryParse(reader.GetString(), out var number))
-                    return number;
+                return number;
             }
 
             return reader.GetInt64();
@@ -32,10 +24,6 @@ namespace System.Text.Json.Serialization
         {
             if (reader.TokenType == JsonTokenType.String)
             {
-                //ReadOnlySpan<byte> span = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
-                //if (Utf8Parser.TryParse(span, out long number, out int bytesConsumed) && span.Length == bytesConsumed)
-                //    return number;
-
                 if (long.TryParse(reader.GetString(), out var number))
                     return number;
 
