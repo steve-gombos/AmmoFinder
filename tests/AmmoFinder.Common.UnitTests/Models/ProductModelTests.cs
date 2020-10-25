8662df67c138e005a4b3f7b86d2043cdabeea373
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2020-10-25 17:45:06 -0400

    Added model unit tests.

---
+++ tests/AmmoFinder.Common.UnitTests/Models/ProductModelTests.cs
@@ -0,0 +1,260 @@
+﻿using AmmoFinder.Common.Models;
+using System;
+using Xunit;
+
+namespace AmmoFinder.Common.UnitTests.Models
+{
+    public class ProductModelTests
+    {
+        [Fact]
+        public void ProductId_IsValid()
+        {
+            // Arrange
+            var expected = 1;
+            var data = new ProductModel
+            {
+                ProductId = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.ProductId);
+        }
+
+        [Fact]
+        public void Name_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Name = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Name);
+        }
+
+        [Fact]
+        public void CreatedOn_IsValid()
+        {
+            // Arrange
+            var expected = DateTime.Now;
+            var data = new ProductModel
+            {
+                UpdatedOn = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.UpdatedOn);
+        }
+
+        [Fact]
+        public void Brand_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Brand = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Brand);
+        }
+
+        [Fact]
+        public void Caliber_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Caliber = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Caliber);
+        }
+
+        [Fact]
+        public void Casing_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Casing = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Casing);
+        }
+
+        [Fact]
+        public void Description_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Description = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Description);
+        }
+
+        [Fact]
+        public void Grain_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Grain = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Grain);
+        }
+
+        [Fact]
+        public void Inventory_IsValid()
+        {
+            // Arrange
+            var expected = 1;
+            var data = new ProductModel
+            {
+                Inventory = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Inventory);
+        }
+
+        [Fact]
+        public void IsAvailable_IsValid()
+        {
+            // Arrange
+            var expected = true;
+            var data = new ProductModel
+            {
+                IsAvailable = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.IsAvailable);
+        }
+
+        [Fact]
+        public void Price_IsValid()
+        {
+            // Arrange
+            var expected = 1.99m;
+            var data = new ProductModel
+            {
+                Price = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Price);
+        }
+
+        [Fact]
+        public void RetailerId_IsValid()
+        {
+            // Arrange
+            var expected = 1;
+            var data = new ProductModel
+            {
+                RetailerId = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RetailerId);
+        }
+
+        [Fact]
+        public void Retailer_IsValid()
+        {
+            // Arrange
+            var data = new ProductModel
+            {
+                Retailer = new RetailerModel()
+            };
+
+            // Assert
+            Assert.IsType<RetailerModel>(data.Retailer);
+        }
+
+        [Fact]
+        public void RetailerProductId_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                RetailerProductId = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RetailerProductId);
+        }
+
+        [Fact]
+        public void RoundContainer_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                RoundContainer = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RoundContainer);
+        }
+
+        [Fact]
+        public void RoundCount_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                RoundCount = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RoundCount);
+        }
+
+        [Fact]
+        public void RoundType_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                RoundType = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RoundType);
+        }
+
+        [Fact]
+        public void Url_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new ProductModel
+            {
+                Url = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Url);
+        }
+    }
+}
---
+++ tests/AmmoFinder.Common.UnitTests/Models/RetailerModelTests.cs
@@ -0,0 +1,69 @@
+﻿using AmmoFinder.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AmmoFinder.Common.UnitTests.Models
+{
+    public class RetailerModelTests
+    {
+        [Fact]
+        public void RetailerId_IsValid()
+        {
+            // Arrange
+            var expected = 1;
+            var data = new RetailerModel
+            {
+                RetailerId = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.RetailerId);
+        }
+
+        [Fact]
+        public void Name_IsValid()
+        {
+            // Arrange
+            var expected = "test";
+            var data = new RetailerModel
+            {
+                Name = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.Name);
+        }
+
+        [Fact]
+        public void CreatedOn_IsValid()
+        {
+            // Arrange
+            var expected = DateTime.Now;
+            var data = new RetailerModel
+            {
+                CreatedOn = expected
+            };
+
+            // Assert
+            Assert.Equal(expected, data.CreatedOn);
+        }
+
+        [Fact]
+        public void Products_IsValid()
+        {
+            // Arrange
+            var data = new RetailerModel
+            {
+                Products = new List<ProductModel>
+                {
+                    new ProductModel()
+                }
+            };
+
+            // Assert
+            Assert.True(data.Products.Any());
+        }
+    }
+}
